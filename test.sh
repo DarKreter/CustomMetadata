e930a1504c4203c3d61a26a25c56b3a714e83026
DarKreter <darkreter@gmail.com>
2023-01-27 11:36:18 +0100

    This time script is working

--- test.sh
+++ test.sh
@@ -5,33 +5,35 @@ series_title=$1
 season_number=$2
 new_season_title=$3
 
-# sql query
-sql_statement()
+# sql read series id query
+sql_read_series_id()
 {
   echo "
-WITH SERIES AS( 
-SELECT 
-    id, title
-FROM 
+SELECT
+    id
+FROM
     metadata_items
-WHERE
-    title=\"$series_title\"
-)
-
-
-UPDATE
-    metadata_items
-SET
-    title=\"$new_season_title\"
-FROM 
-    metadata_items AS SEASON, SERIES
-WHERE
-    SEASON.parent_id = SERIES.id AND
-    metadata_items.[index] = $season_number
+WHERE 
+    title='$series_title'
 ;"
 }
 
 # Run plex-sqlite interpreter on main database
 cd /usr/lib/plexmediaserver/
-./Plex\ Media\ Server --sqlite /config/Library/Application\ Support/Plex\ Media\ Server/Plug-in\ Support/Databases/com.plexapp.plugins.library.db < <(sql_statement)
+series_id=$(./Plex\ Media\ Server --sqlite /config/Library/Application\ Support/Plex\ Media\ Server/Plug-in\ Support/Databases/com.plexapp.plugins.library.db < <(sql_read_series_id))
 
+
+sql_modify_season()
+{
+  echo "
+UPDATE
+    metadata_items
+SET
+    title='$new_season_title'
+WHERE 
+    parent_id='$series_id' AND
+    [index]=$season_number
+;"
+}
+
+./Plex\ Media\ Server --sqlite /config/Library/Application\ Support/Plex\ Media\ Server/Plug-in\ Support/Databases/com.plexapp.plugins.library.db < <(sql_modify_season)
